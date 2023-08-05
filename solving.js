0c623791a6ffa8deb142690f8bcc0302425a5760
nasirmondol <nasirahmed8202@gmail.com>
2023-08-05 23:38:27 +0600

    inital commit

---
+++ solving.js
@@ -0,0 +1,145 @@
+//  ১. তোমাকে যদি বলি একটা স্ট্রিং টাইপের ভেরিয়েবল লিখো। একটা বুলিয়ান টাইপের ভেরিয়েবল লিখো বা একটা নাম্বার টাইপের ভেরিয়েবল লিখো। সেটা কি তুমি লিখতে পারবে? যদি পারো তাহলে ভিজুয়াল ষ্টুডিও কোড খুলে লিখে ফেলো। 
+
+// Problem 1.
+// String variable
+const name = 'Jahid Ahsan';
+// boolean type variable
+const isMoney = true;
+// Number type variable
+const roll = 1;
+
+// problem 2
+// ২. তুমি কি জানো কখন let দিয়ে ভেরিয়েবল লিখতে হয়ে আর কখন const দিয়ে ভেরিয়েবল লিখতে হয়। যদি লিখতে পারো তাহলে লিখে ফেলো। এবং যেই যেটা দিয়ে ভেরিয়েবল লিখলে আবার সেটা চেইঞ্জ করা যায়। তাহলে যেটা দিয়ে ভেরিয়েবল ডিক্লেয়ার করলে চেইঞ্জ করা যায়। সেটার মান চেইঞ্জ করে নাও । অর্থাৎ জাস্ট দুইটা ভেরিয়েবল ডিক্লেয়ার করবে let এবং const দিয়ে। তারপর যেকোন একটার ভ্যালু চেইঞ্জ করবে। এই let এবং const মাথা গরম করে দিলে টেনশন নিয়ে না। সামনের মাইলস্টোন এর পরের মাইলস্টোন এ এইটা নিয়ে আরো ডিসকাস করা হবে। 
+
+
+const myName = 'Abid Hasan';
+
+let phonePrice = 20000;
+phonePrice = 30000;
+
+//  problem 3
+// ৩. দুইটা ভেরিয়েবল এর মধ্যে যোগ, বিয়োগ, গুণ, ভাগ কিভাবে করতে হয় সেটা কি জানো। অর্থাৎ তুমি কি +, -, *, /, %এইগুলার ব্যবহার জানো। তাহলে নাম্বার টাইপের দুইটা ভেরিয়েবল লিখো তারপর তাদের যোগ করে সেটার মান আরেকটা ভেরিয়েবল এ রাখো। একইভাবে ওই দুইটা ভেরিয়েবল এর মধ্যে বিয়োগ, গুন, ভাগ এবং ভাগশেষ বের করো। 
+
+const num1 = 50;
+const num2 = 10;
+const add = num1 + num2;
+// console.log(add)
+const different = num1 - num2
+// console.log(different);
+
+// const multiplication = num1 * num2;
+// console.log(multiplication)
+
+const dievided = num1 % num2;
+// console.log(dievided)
+
+// ৪. তুমি কি দুইটা ভেরিয়েবল এরমধ্যে তুলনা করতে পারো। কম্পারিজন করতে পারো। যে দুইটা ভেরিয়েবল এর মধ্যে প্রথমটা সেকেন্ডটা এর চাইতে ছোট, বড়, অসমান, সমান , ছোট বা সমান, বড় বা সমান। এইগুলা চেক করতে পারো। অর্থাৎ <, >, ==, !=, <=, >= চিহ্নগুলা ব্যবহার করতে পারো। তাহলে দুইটা সংখ্যা টাইপের ভেরিয়েবল ডিক্লেয়ার করে তাদেরকে এই ছয়ভাবে তুলনা করে কোড লিখে ফেলো। 
+
+// const firstNumber = 10;
+// const secondNumber = 30;
+// if(firstNumber == secondNumber){
+//     console.log('They are not equal')
+// }
+// else{
+//     console.log('they are double')
+// }
+
+// ৫. তোমার যদি দুইটা শর্ত পূরণ করতে বলে। এবং দুইটা শর্তের মধ্যে দুইটাই পূরণ করতে হবে। তাহলে তুমি কি সেটা চেক করতে পারবে? একইভাবে যদি বলে তুমি দুইটা শর্তের যেকোন একটা পূরণ করতে পারবে। অর্থাৎ তুমি && এবং || এর ব্যবহার করতে পারো কিনা। যদি পারো তাহলে নিজে নিজে এই রকমের কোড লিখে ফেলো। 
+
+// const firstNumber = 10;
+// const secondNumber = 30;
+
+// if(firstNumber == secondNumber || firstNumber < secondNumber){
+//     console.log('first number is small then second number')
+// }
+// else{
+//     console.log('They are not same')
+// }
+
+// ৬. তুমি কি একটা শর্ত পালন করলে একটা কিছু করবে। শর্ত পূরণ না করলে অন্য কিছু একটা করবে এমন কোড লিখতে পারবে। অর্থাৎ তুমি কি if-else এর কোড লিখতে পারবে। পারলে একটা কোড লিখে ফেলো 
+
+// const name11 = 'Rajib';
+// if(name == 'Rajib'){
+//     console.log('This is rajib')
+// }
+// else{
+//     console.log("This is not rajib ")
+// }
+
+// ৭. তোমাকে যদি বলে একটা while লুপ দিয়ে ৭ থেকে ১৯ পর্যন্ত যতগুলা বিজোড় সংখ্যা আছে সেগুলা দেখাতে। তুমি কি সেটা দেখাতে পারবে? পারলে সেই কোড লিখে ফেলো। 
+
+// let i = 1;
+// while (i <= 19) {
+//     if (i % 2 !== 0) {
+//         console.log(i)
+
+//     }
+//     i++;
+// }
+
+
+// for (let i = 7; i <= 19; i++) {
+//     if (i % 2 !== 0) {
+//         console.log(i, 'is odd');
+//     }
+// }
+
+
+// ৮. তোমাকে যদি বলা হয় তুমি একটা array ডিক্লেয়ার করবে। এবং সেটার মধ্যে কয়টা উপাদান আছে সেটা বের করবে হবে। সেই array এর এর মধ্যে চতুর্থ পজিশন এর উপাদান চেইঞ্জ করতে হবে। array এর মধ্যে দুইটা উপাদান যোগ করতে হবে। এবং একটা উপাদান কে বের করে দিতে হবে। তুমি কি সেটা করতে পারবে। 
+
+// const numberArray = [11, 45, 30, 56, 90, 20, 44]
+// const arrLength = numberArray.length;
+// // console.log(arrLength);
+// numberArray[4] = 400;
+// numberArray.push(100, 200);
+// // console.log(numberArray)
+// numberArray.pop();
+// console.log(numberArray);
+
+// ৯. তুমি কি একটা ফর লুপ দিয়ে কোন একটা array এর সবগুলা উপাদানকে দেখাতে পারবে। সেটা রেগুলার for লুপ হোক বা for of হোক। হলে সেই স্টাইলে একটা কোড লিখে ফেলো। 
+// const numbers = [11, 45, 30, 56, 90, 20, 44];
+// for(let i = 0;  i < numbers.length; i++){
+//     console.log(numbers[i])
+// }
+
+// ১০. তোমাকে যদি বলা হয়। একটা array এর মধ্যে ৮০ এর চাইতে বড় সংখ্যা থাকলে সেগুলাকে console log করে দেখাতে সেটা কি তুমি পারবে? তাহলে তুমি সেই কোড করে ফেলো|
+
+// const numbers = [11, 45, 30, 56, 90, 100, 120, 399, 20, 44];
+
+// for(let i = 0; i < numbers.length; i++){
+//     if(numbers[i] > 80){
+//         console.log([numbers[i]])
+//     }
+// }
+
+// . তিনটা সংখ্যার গুনফল বের করে ফাইনাল রেজাল্ট আউটপুট হিসেবে রিটার্ন করতে হবে। তুমি কি সেই কোড লিখতে পারবে। যদি পারো তাহলে সেই কোড লিখে ফেলো। 
+const number1 = 10;
+const number2 = 20;
+const number3 = 120;
+
+// function multiplicationThreeNumber(number1, number2, number3) {
+//     const multiplication = number1 * number2 * number3
+//     return multiplication;
+// }
+
+
+// const multiplication = multiplicationThreeNumber(10, 12, 5);
+
+// console.log(multiplication);
+
+// ১২. একটা অবজেক্ট ডিক্লেয়ার করবে। সেটাতে তিনটা প্রপার্টি থাকবে। এবং কোন একটা প্রোপার্টি এর মান চেইঞ্জ করবা। 
+const productDetails = {
+    name: 'Laptop',
+    price: 67900,
+    color: 'Black',
+    ram: 4,
+    monitor: '14inch'
+
+}
+
+// productDetails.price = 80000;
+productDetails['color'] = 'white pink';
+console.log(productDetails)
+
+
+
